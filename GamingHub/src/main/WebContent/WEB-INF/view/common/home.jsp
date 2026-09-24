1c8d848673afb6d9ca8b98199a594282eff4210f
raffa <raffa@RaffaeleF>
2026-09-24 18:33:48 +0200

    pagina home (prototipo) e servlet per la home

---
+++ GamingHub/src/main/WebContent/WEB-INF/view/common/home.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Gaming Hub</title>
+</head>
+<body>
+
+<h1>Gaming Hub</h1>
+
+<p>Work in progress</p>
+
+</body>
+</html>
---
+++ GamingHub/src/main/java/control/HomeServlet.java
@@ -0,0 +1,23 @@
+package control;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.getRequestDispatcher(
+                "/WEB-INF/view/common/home.jsp")
+                .forward(request, response);
+    }
+}
